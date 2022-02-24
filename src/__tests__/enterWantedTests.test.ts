d17e99dbf99aac3aebc2b21926fef5703d0f2db5
Derek Hendricks <derek.m.hendricks>
2022-02-24 02:54:26 -0700

    mke input tests and edit assertions

--- src/__tests__/enterWantedTests.test.ts
+++ src/__tests__/enterWantedTests.test.ts
@@ -3,8 +3,7 @@ import {
   By,
   Capabilities,
   until,
-  WebDriver,
-  WebElement
+  WebDriver
 } from "selenium-webdriver";
 
 const chromedriver = require("chromedriver");
@@ -14,25 +13,49 @@ const driver: WebDriver = new Builder()
   .build();
 
 class EnterWantedPageClass {
+  url: string = "https://devmountain-qa.github.io/enter-wanted/1.4_Assignment/index.html";
+  driver: WebDriver;
+  headerInput: By = By.css("input[name=hdrInput]");
+  mkeInput: By = By.css("input[name=mkeInput]");
+  submitButton: By = By.css("#saveBtn");
+  headerErrorMessage: By = By.xpath(`//*[text() = 'The "Header" field should be between 9 and 19 characters long.']`);
+  mkeErrorMessage: By = By.xpath(`//*[text() = 'The "MKE" field should be between 2 and 4 characters long.']`);
+  
+  /**
+   * Creates an instance of EnterWantedPageClass.
+   * @param {WebDriver} driver - webDriver client to control browser
+   */
   constructor(driver: WebDriver) {
     this.driver = driver;
   }
 
-  url: string = "https://devmountain-qa.github.io/enter-wanted/1.4_Assignment/index.html";
-  driver: WebDriver;
-  headerInput: By = By.css("input[name=hdrInput]");
-  submitButton: By = By.css("#saveBtn");
-  headerErrorMessage: By = By.xpath(`//*[text() = 'The "Header" field should be between 9 and 19 characters long.']`);
-
-
-  public async clickSubmit(): Promise<void> {
+  /**
+   * Clicks the submit button on the enter-wanted page.
+   * @returns Promise void
+   */
+   public async clickSubmit(): Promise<void> {
     await driver.wait(until.elementLocated(this.submitButton));
     let submitButton = await driver.findElement(this.submitButton);
     await submitButton.click();
   }
 
-  public async enterHeaderInput(text: string): Promise<void> {
-    const headerTextValue = text;
+    /**
+   * fills the "mke" input field with the string value passed in the parameter
+   * @param {string} mkeTextValue - text input value for the mke input field
+   * @returns Promise void
+   */
+     public async enterMkeInput(mkeTextValue: string): Promise<void> {
+      await driver.wait(until.elementLocated(this.mkeInput));
+      let mkeElement = await driver.findElement(this.mkeInput);
+      await mkeElement.sendKeys(mkeTextValue);
+    }
+
+  /**
+   * fills the "header" input field with the string value passed in the parameter
+   * @param {string} headerTextValue - text input value for the header input field
+   * @returns Promise void
+   */
+  public async enterHeaderInput(headerTextValue: string): Promise<void> {
     await driver.wait(until.elementLocated(this.headerInput));
     let headerElement = await driver.findElement(this.headerInput);
     await headerElement.sendKeys(headerTextValue);
@@ -41,6 +64,7 @@ class EnterWantedPageClass {
 
 describe("the todo app", () => {
   const enterWantedPage: EnterWantedPageClass = new EnterWantedPageClass(driver);
+
   beforeEach(async () => {
     await driver.get(enterWantedPage.url);
   });
@@ -52,8 +76,8 @@ describe("the todo app", () => {
   it("The header field does not accept a value that is less than 9 characters in length", async () => {
     await enterWantedPage.enterHeaderInput("qwertyui");
     await enterWantedPage.clickSubmit();
-    let headerErrorMessage = await driver.findElement(enterWantedPage.headerErrorMessage).getText();
-    expect(headerErrorMessage).toBe('The "Header" field should be between 9 and 19 characters long.');
+    let headerErrorMessage = await driver.findElements(enterWantedPage.headerErrorMessage);
+    expect(headerErrorMessage.length).toBe(1);
   });
 
 // https://dmutah.atlassian.net/browse/DH6DL-35
@@ -68,7 +92,32 @@ it("The header field accepts a value that is between 9 and 19 characters in leng
 it("The header field does not accept a value that is greater than 19 characters in length", async () => {
   await enterWantedPage.enterHeaderInput("qwertyuiopqwertyuiop");
   await enterWantedPage.clickSubmit();
-  let headerErrorMessage = await driver.findElement(enterWantedPage.headerErrorMessage).getText();
-  expect(headerErrorMessage).toBe('The "Header" field should be between 9 and 19 characters long.');
+  let headerErrorMessage = await driver.findElements(enterWantedPage.headerErrorMessage);
+  expect(headerErrorMessage.length).toBe(1);
 });
+
+// TODO: create jira test case and add link here
+it("The MKE field does not accept a value that is less than 2 characters in length", async () => {
+  await enterWantedPage.enterMkeInput("q");
+  await enterWantedPage.clickSubmit();
+  let mkeErrorMessage = await driver.findElements(enterWantedPage.mkeErrorMessage);
+  expect(mkeErrorMessage.length).toBe(1);
+});
+
+// TODO: create jira test case and add link here
+it("The MKE field accepts a value that is between 2 and 4 characters in length", async () => {
+  await enterWantedPage.enterMkeInput("qw");
+  await enterWantedPage.clickSubmit();
+  let mkeErrorMessage = await driver.findElements(enterWantedPage.mkeErrorMessage);
+  expect(mkeErrorMessage.length).toBe(0);
+});
+
+// TODO: create jira test case and add link here
+it("The MKE field does not accept a value that is greater than 4 characters in length", async () => {
+  await enterWantedPage.enterMkeInput("qwert");
+  await enterWantedPage.clickSubmit();
+  let mkeErrorMessage = await driver.findElements(enterWantedPage.mkeErrorMessage);
+  expect(mkeErrorMessage.length).toBe(1);
+});
+
 });
